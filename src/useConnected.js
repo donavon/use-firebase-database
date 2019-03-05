956daf779683e8dd191c9bfd4dc6a59bf3115759
donavon <github@donavon.com>
2019-03-05 17:29:28 -0500

    fix useConnected

--- src/useConnected.js
+++ src/useConnected.js
@@ -1,17 +1,8 @@
 import useValue from './useValue';
 
 const useConnected = () => {
-  const [connected] = useValue('./info/connected');
+  const [connected] = useValue('/.info/connected');
   return connected;
 };
 
 export default useConnected;
-
-// var connectedRef = firebase.database().ref(".info/connected");
-// connectedRef.on("value", function (snap) {
-//   if (snap.val() === true) {
-//     alert("connected");
-//   } else {
-//     alert("not connected");
-//   }
-// });
